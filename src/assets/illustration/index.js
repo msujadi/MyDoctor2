919f0575c7fdde47ca0139ebaf41bf7f63052f10
msujadi <42762729+msujadi@users.noreply.github.com>
2021-10-13 09:58:25 +0700

    test push 1

---
+++ src/assets/illustration/index.js
@@ -0,0 +1,4 @@
+import ILLogo from './logo.svg';
+import ILGetStarted from './get-started.jpg';
+
+export {ILLogo, ILGetStarted};
---
+++ src/pages/GetStarted/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import {ILGetStarted, ILLogo} from '../../assets';
+import {Button, Gap} from '../../components';
+
+function GetStarted() {
+  return (
+    <ImageBackground source={ILGetStarted} style={styles.page}>
+      <View>
+        <ILLogo />
+        <Text style={styles.title}>
+          Konsultasi dengan dokter jadi lebih mudah & fleksibel
+        </Text>
+      </View>
+      <View>
+        <Button title="Get Started" />
+        <Gap height={16} />
+        <Button type="secondary" title="Sign In" />
+      </View>
+    </ImageBackground>
+  );
+}
+
+export default GetStarted;
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 40,
+    justifyContent: 'space-between',
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: 'Nunito-SemiBold',
+    color: 'white',
+    marginTop: 91,
+  },
+});
