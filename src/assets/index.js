03c81a8bd44cf600961eb9918b4f8ee7543e995e
msujadi <42762729+msujadi@users.noreply.github.com>
2021-10-13 10:04:01 +0700

    test 2 abis replace files

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,7 @@
 // export * from './dummy';
+<<<<<<< HEAD
+export * from './icon';
+=======
 // export * from './icon';
+>>>>>>> 20a98ddac87308b684aa0a142bf1acb470177752
 export * from './illustration';
